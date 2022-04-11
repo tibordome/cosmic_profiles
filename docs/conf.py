2589d1d68114af8277617f18087fc75e50dfbad8
tibordome <tibor.doeme@alumni.ethz.ch>
2022-04-11 16:45:49 +0100

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../cosmic_shapes/for_docs'))
-sys.path.insert(0, os.path.abspath('../../example_scripts'))
+sys.path.insert(0, os.path.abspath('../cosmic_shapes/for_docs'))
+sys.path.insert(0, os.path.abspath('../example_scripts'))
 
 
 # -- Project information -----------------------------------------------------
