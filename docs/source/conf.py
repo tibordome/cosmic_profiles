4cac5d186286e8020652784a6fb523255157b512
tibordome <tibor.doeme@alumni.ethz.ch>
2022-04-11 18:23:14 +0100

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
     'sphinx.ext.autodoc',
     'sphinx_rtd_theme',
     'sphinx.ext.inheritance_diagram',
