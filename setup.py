71632a91a97f3ff82096512b952ebc5796cc792b
tibordome <tibor.doeme@alumni.ethz.ch>
2022-04-24 23:49:34 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,12 @@ ext_modules=[Extension(
                 extra_link_args=["-fopenmp"],
                 include_dirs=[np.get_include(), '.']
             ), Extension(
+                "cosmic_shapes.gen_csh_gx_cat",
+                sources=['cosmic_shapes/gen_csh_gx_cat.pyx'],
+                extra_compile_args=["-fopenmp"],
+                extra_link_args=["-fopenmp"],
+                include_dirs=[np.get_include(), '.']
+            ), Extension(
                 "cosmic_shapes.cosmic_shapes",
                 sources=['cosmic_shapes/cosmic_shapes.c'],
                 extra_compile_args=["-fopenmp"],
@@ -52,7 +58,7 @@ ext_modules=[Extension(
 # This call to setup() does all the work
 setup(
     name="cosmic_shapes",
-    version="1.8.0",
+    version="1.9.0",
     description="Implements various ellipsoidal shape identification algorithms for 3D particle data",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -70,7 +76,7 @@ setup(
     ],
     cmdclass={'build_ext': BuildExtWithoutPlatformSuffix},
     ext_modules = cythonize(ext_modules),
-    packages=["cosmic_shapes"],
+    packages=find_packages(),
     include_package_data=True,
     include_dirs=np.get_include(),
     install_requires=["cython", "numpy>=1.19.2", "scikit-learn", "mpi4py", "h5py", "matplotlib"]
